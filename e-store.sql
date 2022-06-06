8d8f15becc864092324fcd65afd4874fad26525b
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-06-06 21:12:38 +0700

    Update database sql

--- e-store.sql
+++ e-store.sql
@@ -1,6 +1,11 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+DROP TABLE IF EXISTS barang;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS petugas;
+DROP TABLE IF EXISTS penjualan;
+DROP TABLE IF EXISTS detail_penjualan;
 CREATE TABLE `barang` (
   `id_barang` int(11) NOT NULL,
   `nama_barang` varchar(50) NOT NULL,
@@ -27,7 +32,9 @@ CREATE TABLE `detail_penjualan` (
 CREATE TABLE `penjualan` (
   `id_penjualan` int(11) NOT NULL,
   `id_customer` int(11) NOT NULL,
-  `id_petugas` int(11) DEFAULT NULL
+  `id_petugas` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 CREATE TABLE `petugas` (
   `id_petugas` int(11) NOT NULL,
@@ -67,30 +74,86 @@ ADD CONSTRAINT `detail_penjualan_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `b
 ALTER TABLE `penjualan`
 ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`),
   ADD CONSTRAINT `penjualan_ibfk_2` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`);
-
-INSERT INTO barang VALUES
-(NULL, "Fanta", NULL, "commodi nesciunt enim quae eligendi", 12000, 10), 
-(NULL, "Sprite", NULL, "commodi nesciunt enim quae eligendi", 11000, 10), 
-(NULL, "Coca-cola", NULL, "commodi nesciunt enim quae eligendi", 11000, 10);
-
-INSERT INTO customer VALUES
-(NULL, "wahyudi@gmail.com", "wahyudiok", MD5("wahyudi23"), "Malang", "1953-05-27"),
-(NULL, "agus@gmail.com", "agusok", MD5("agus2345"), "Singosari", "1983-08-21"),
-(NULL, "roqib@gmail.com", "roqibroqib", MD5("roqib23@"), "Blitar", "1988-09-13");
-
-INSERT INTO petugas VALUES
-(NULL, "aryanto@gmail.com", "aryanto", MD5("aryanto666"), "Pakis", "1943-05-27"),
-(NULL, "yuli@gmail.com", "yuliii", MD5("yulilestari"), "Singosari", "1933-08-21"),
-(NULL, "aman@gmail.com", "aman", MD5("aman"), "Sawojajar", "1952-09-13");
-
-
-INSERT INTO penjualan VALUES (NULL, 1, NULL);
-
-INSERT INTO detail_penjualan VALUES (NULL, 1, 1, 2, NULL);
-
+INSERT INTO barang
+VALUES (
+    NULL,
+    "Fanta",
+    NULL,
+    "commodi nesciunt enim quae eligendi",
+    12000,
+    10
+  ),
+  (
+    NULL,
+    "Sprite",
+    NULL,
+    "commodi nesciunt enim quae eligendi",
+    11000,
+    10
+  ),
+  (
+    NULL,
+    "Coca-cola",
+    NULL,
+    "commodi nesciunt enim quae eligendi",
+    11000,
+    10
+  );
+INSERT INTO customer
+VALUES (
+    NULL,
+    "wahyudi@gmail.com",
+    "wahyudiok",
+    MD5("wahyudi23"),
+    "Malang",
+    "1953-05-27"
+  ),
+  (
+    NULL,
+    "agus@gmail.com",
+    "agusok",
+    MD5("agus2345"),
+    "Singosari",
+    "1983-08-21"
+  ),
+  (
+    NULL,
+    "roqib@gmail.com",
+    "roqibroqib",
+    MD5("roqib23@"),
+    "Blitar",
+    "1988-09-13"
+  );
+INSERT INTO petugas
+VALUES (
+    NULL,
+    "aryanto@gmail.com",
+    "aryanto",
+    MD5("aryanto666"),
+    "Pakis",
+    "1943-05-27"
+  ),
+  (
+    NULL,
+    "yuli@gmail.com",
+    "yuliii",
+    MD5("yulilestari"),
+    "Singosari",
+    "1933-08-21"
+  ),
+  (
+    NULL,
+    "aman@gmail.com",
+    "aman",
+    MD5("aman"),
+    "Sawojajar",
+    "1952-09-13"
+  );
+INSERT INTO penjualan (id_customer) VALUES (1), (2);
+INSERT INTO detail_penjualan
+VALUES (NULL, 1, 1, 2, NULL);
 /*Update total harga*/
-UPDATE detail_penjualan 
-INNER JOIN barang ON detail_penjualan.id_barang = barang.id_barang 
-SET detail_penjualan.total = detail_penjualan.jumlah*barang.harga;
-
+UPDATE detail_penjualan
+  INNER JOIN barang ON detail_penjualan.id_barang = barang.id_barang
+SET detail_penjualan.total = detail_penjualan.jumlah * barang.harga;
 COMMIT;
